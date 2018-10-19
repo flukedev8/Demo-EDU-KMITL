7c713d9cf63543b3e66e08536411181ce54b1f30
sittisak  plianklip <sittisak.951753@gmail.com>
2018-10-20 00:45:45 +0700

    Test Backend  authen

--- server/src/app.js
+++ server/src/app.js
@@ -6,13 +6,13 @@ const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('combine'))
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hello world!'
+    message: `Welcom in websit ${req.body.email} Have fun! and ${req.body.password}`
   })
 })
 
